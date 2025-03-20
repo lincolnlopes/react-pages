b36fb8eaef7df25232f807873605ed1195b25af0
Lincoln Lopes <lincolnlopes@msn.com>
2025-03-20 01:12:31 -0300

    Update Vite configuration to use VITE_BASE_PATH and add .env to .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
   return {
-    plugins: [react()],
-    base: env.BASE_PATH || '/react-pages/',// Importante para GitHub Pages
+    base: env.VITE_BASE_PATH || '/react-pages/', // Importante para GitHub Pages,
+    plugins: [react()]
   }
 })
