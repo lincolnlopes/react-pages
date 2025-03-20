9d0a6b48efe12a95900007579f11f2e5041f8660
Lincoln Lopes <lincolnlopes@msn.com>
2025-03-20 00:48:28 -0300

    Add @types/node dependency and update Vite configuration to load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/react-pages/', // Importante para GitHub Pages
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [react()],
+    base: env.BASE_PATH || '/react-pages/',// Importante para GitHub Pages
+  }
 })
