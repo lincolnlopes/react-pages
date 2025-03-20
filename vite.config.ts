db7a5f6ded8141b067af26bf763424f5fe5a75ec
Lincoln Lopes <lincolnlopes@msn.com>
2025-03-19 23:49:05 -0300

    Update Vite configuration base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para GitHub Pages
+  base: './react-pages/', // Importante para GitHub Pages
 })
